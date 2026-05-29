9bc2a150e7e95921da3d09a632d922b35d7841d0
proteincarp902 <proteincarp902@gmail.com>
2026-05-29 19:01:35 +0300

    Add Firebase configuration and Firestore initialization
    Added Firebase configuration and initialized Firestore.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDNpvmUTe5cDQUg2Fk49NPumpIgOO-8e00",
+  authDomain: "protincarps.firebaseapp.com",
+  projectId: "protincarps",
+  storageBucket: "protincarps.firebasestorage.app",
+  messagingSenderId: "68975799324",
+  appId: "1:68975799324:web:af819448ebfcaa9ffb43d9",
+  measurementId: "G-SHFZR3PSCW"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
